02920be9757f7c1ff33d5a904142bd5bb0600bf3
jean carlos <jeancarlos3102@gmail.com>
2024-05-12 14:31:32 -0400

    delete session store and fix pagination when filter data

--- datatable.js
+++ datatable.js
@@ -6,6 +6,8 @@ const thead = document.getElementById('thead');
 const tfoot = document.getElementById('tfoot');
 const showing = document.getElementById('showing');
 const tableName = 'dataTable';
+let savedData = [];
+let saveFiltered = [];
 let currentPage = 1;
 let itemPerPage = entriesPagination.value;
 let sortBy = '';
@@ -15,27 +17,37 @@ const getUsers = async () => {
 	return await response.json();
 };
 
-const pagination = (filterData) => {
+const pagination = () => {
 	const page = parseInt(currentPage);
 	const item = parseInt(itemPerPage);
-	let values = getValueFromLocalStore(tableName);
+	let data = savedData.body;
 
-	if (filterData) values = filterData;
+	if (saveFiltered.length > 0) {
+		data = saveFiltered;
+	}
 
-	if (values.dataTable) {
-		const startIndex = (page - 1) * item;
-		const endIndex = item + startIndex;
+	if (data) {
+		let startIndex = (page - 1) * item;
+		let endIndex = item + startIndex;
 
-		showing.innerHTML = `Showing ${startIndex + 1} to ${
-			values.dataTable.length
-		} of ${getValueFromLocalStore(tableName).dataTable.length} entries`;
+		if (data.length < startIndex) {
+			startIndex = 0;
+			endIndex = item + startIndex;
+		}
 
-		const result = values.dataTable.slice(startIndex, endIndex);
+		let result = data.slice(startIndex, endIndex);
 
+		showingEntries(startIndex, data);
 		if (sortBy) return sorbByElement(result);
 
 		return result;
 	}
+};
+
+const showingEntries = (startIndex, data) => {
+	showing.innerHTML = `Showing ${startIndex + 1} to ${data.length} of ${
+		savedData.body.length
+	} entries`;
 };
 
 const sorbByElement = (result) => {
@@ -48,21 +60,6 @@ const sorbByElement = (result) => {
 	});
 };
 
-const generateHeader = (head) => {
-	let headRow = `<tr>`;
-	let footRow = `<tr>`;
-
-	headRow += `<th>#</th>`;
-	footRow += `<th>#</th>`;
-	for (const key in head) {
-		headRow += `<th>${head[key]}<span class="${head[key]}"> ^ </span></th>`;
-		footRow += `<th>${head[key]}</th>`;
-	}
-	headRow += `</tr>`;
-	thead.innerHTML = headRow;
-	tfoot.innerHTML = footRow;
-	handleSort();
-};
 
 const handleSort = () => {
 	const thElements = thead.querySelectorAll('th');
@@ -87,38 +84,9 @@ const clearSorbSelector = () => {
 	});
 };
 
-const generateBody = (head, body) => {
-	let bodyRow = '';
-
-	if (body.length > 0) {
-		for (const key in body) {
-			const object = body[key];
-
-			bodyRow += `<tr>`;
-			bodyRow += `<td>${parseInt(key) + 1}</th>`;
-			for (const iterator of head) {
-				bodyRow += `<td>${object[iterator]}</th>`;
-			}
-
-			bodyRow += `</tr>`;
-		}
-	} else {
-		bodyRow += `<td colspan="${
-			head.length + 1
-		}">No matching records found</td>`;
-	}
-
-	tbody.innerHTML = bodyRow;
-};
-
-const getValueFromLocalStore = (value) => {
-	const data = sessionStorage.getItem(value);
-	if (data) return JSON.parse(data);
-};
-
 const handleNext = () => {
 	const values = getValueFromLocalStore(tableName);
-	if (currentPage > 0 && values.dataTable) {
+	if (currentPage > 0 && savedData.body) {
 		currentPage += 1;
 		const paginatedData = pagination();
 		if (paginatedData.length > 0) {
@@ -134,14 +102,27 @@ const handleNext = () => {
 };
 
 const handlePrevious = () => {
-	const values = getValueFromLocalStore(tableName);
-	if (currentPage > 1 && values.dataTable) {
+	if (currentPage > 1 && savedData.body) {
 		currentPage -= 1;
 
 		const paginatedData = pagination();
 		if (paginatedData.length > 0) {
 			generateTable(paginatedData);
 			clearPagination();
+			generateTable(paginatedData);
+			generateButtonPagination();
+		}
+	}
+	handleActiveColor();
+};
+
+const handlePage = (value) => {
+	if (savedData.body) {
+		currentPage = parseInt(value);
+		const paginatedData = pagination();
+		if (paginatedData.length > 0) {
+			clearPagination();
+			currentPage = parseInt(value);
 			generateTable(paginatedData);
 			generateButtonPagination();
 		}
@@ -160,6 +141,7 @@ const handleActiveColor = () => {
 		}
 	}
 };
+
 const generateButton = (value, disable) => {
 	const element = document.createElement('li');
 	const button = document.createElement('button');
@@ -178,26 +160,92 @@ const generateButton = (value, disable) => {
 	};
 };
 
-const handlePage = (value) => {
-	const values = getValueFromLocalStore(tableName);
-	if (values) {
-		currentPage = parseInt(value);
-		const paginatedData = pagination();
-		if (paginatedData.length > 0) {
-			clearPagination();
-			currentPage = parseInt(value);
-			generateTable(paginatedData);
-			generateButtonPagination();
+filterData.addEventListener('input', () => {
+	const input = filterData.value.toLowerCase().trim();
+
+	const searchData = savedData.body.filter((item) => {
+		for (const iterator of savedData.head) {
+			const element = item[iterator].toString().toLowerCase().trim();
+			if (element.includes(input)) return item;
 		}
+	});
+
+	saveFiltered = searchData;
+
+	const paginatedData = pagination();
+
+	clearPagination();
+	generateTable(paginatedData);
+	generateButtonPagination();
+});
+
+const clearPagination = () => {
+	const liToDelete = paginationList.querySelectorAll('li');
+	for (const element of liToDelete) {
+		const getList = document.getElementsByClassName('li')[0];
+		getList.remove(element);
 	}
-	handleActiveColor();
 };
 
-const generateButtonPagination = (dataFilter) => {
-	const values = getValueFromLocalStore(tableName);
-	let element = values.dataTable.length / itemPerPage;
-	if (dataFilter) {
-		element = dataFilter.length / itemPerPage;
+
+const generateHeader = (head) => {
+	let headRow = '<tr>';
+	let footRow = '<tr>';
+
+	headRow += '<th>#</th>';
+	footRow += '<th>#</th>';
+	for (const key in head) {
+		headRow += `<th>${head[key]}<span class="${head[key]}"> ^ </span></th>`;
+		footRow += `<th>${head[key]}</th>`;
+	}
+	headRow += '</tr>';
+	thead.innerHTML = headRow;
+	tfoot.innerHTML = footRow;
+	handleSort();
+};
+
+const generateBody = (head, body) => {
+	let bodyRow = '';
+
+	if (body.length > 0) {
+		for (const key in body) {
+			const object = body[key];
+
+			bodyRow += '<tr>';
+			bodyRow += `<td>${parseInt(key) + 1}</th>`;
+			for (const iterator of head) {
+				bodyRow += `<td>${object[iterator]}</th>`;
+			}
+
+			bodyRow += '</tr>';
+		}
+	} else {
+		bodyRow += `<td colspan="${
+			head.length + 1
+		}">No matching records found</td>`;
+	}
+
+	tbody.innerHTML = bodyRow;
+};
+
+entriesPagination.addEventListener('change', () => {
+	let paginatedData = null;
+	const items = parseInt(entriesPagination.value);
+	(currentPage = 1), (itemPerPage = items), (paginatedData = pagination());
+	if (paginatedData.length > 0) {
+		clearPagination();
+		generateTable(paginatedData);
+		itemPerPage = items;
+		generateButtonPagination();
+	}
+});
+
+
+
+const generateButtonPagination = () => {
+	let element = Math.ceil(savedData.body.length / itemPerPage);
+	if (saveFiltered.length > 0) {
+		element = Math.ceil(saveFiltered.length / itemPerPage);
 	}
 
 	const maxElement = currentPage + 5;
@@ -235,27 +283,19 @@ const generateButtonPagination = (dataFilter) => {
 	li.classList.add('active');
 };
 
+
 const generateTable = (body, generateHead = false) => {
-	const values = getValueFromLocalStore(tableName);
-	if (values) {
+	if (body) {
 		if (generateHead) {
-			generateHeader(values.head);
+			generateHeader(savedData.head);
 		}
-		generateBody(values.head, body);
+		generateBody(savedData.head, body);
 	}
-};
-
-const saveData = (data) => {
-	if (sessionStorage.getItem(tableName)) {
-		sessionStorage.removeItem(tableName);
-	}
-
-	if (data) sessionStorage.setItem(tableName, JSON.stringify(data));
 };
 
 const dataTable = (head, data) => {
 	if (data.length > 0) {
-		saveData({ head: head, dataTable: data });
+		savedData = { head: head, body: data };
 		const paginatedData = pagination();
 		if (paginatedData.length > 0) {
 			generateTable(paginatedData, true);
@@ -265,46 +305,6 @@ const dataTable = (head, data) => {
 		generateTable(data);
 	}
 };
-entriesPagination.addEventListener('change', () => {
-	let paginatedData = null;
-	const items = parseInt(entriesPagination.value);
-	(currentPage = 1), (itemPerPage = items), (paginatedData = pagination());
-	if (paginatedData.length > 0) {
-		clearPagination();
-		generateTable(paginatedData);
-		itemPerPage = items;
-		generateButtonPagination();
-	}
-});
-
-const clearPagination = () => {
-	const liToDelete = paginationList.querySelectorAll('li');
-	for (const element of liToDelete) {
-		const getList = document.getElementsByClassName('li')[0];
-		getList.remove(element);
-	}
-};
-
-filterData.addEventListener('input', () => {
-	const input = filterData.value.toLowerCase().trim();
-	const values = getValueFromLocalStore(tableName);
-
-	const searchData = values.dataTable.filter((item) => {
-		for (const iterator of values.head) {
-			const element = item[iterator].toString().toLowerCase().trim();
-			if (element.includes(input)) return item;
-		}
-	});
-
-	const paginatedData = pagination({
-		...values,
-		dataTable: searchData,
-	});
-
-	clearPagination();
-	generateTable(paginatedData);
-	generateButtonPagination(searchData);
-});
 
 const printData = async () => {
 	const data = await getUsers();
